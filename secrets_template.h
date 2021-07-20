863ea5756e9a9e7d32d7f526e4bad4d915eb00b9
Tomáš Macek <tomas.macek@gmail.com>
2021-07-20 16:19:00 +0200

    Merged with display handling and forecast and time from cloud

--- secrets_template.h
+++ secrets_template.h
@@ -1,3 +1,10 @@
+// WIFI
 const char* ssid     = "xxx";
 const char* password = "xxx";
+
 const char* THINGSPEAK_API_KEY = "xxx";
+
+// OpenWeatherMap Settings
+// Sign up here to get an API key:
+// https://docs.thingpulse.com/how-tos/openweathermap-key/
+String OPEN_WEATHER_MAP_APP_ID = "xxx";
